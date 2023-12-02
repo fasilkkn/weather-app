40b51216091e402175731efd0799f0c1bdc682d8
fasilkkn <kknfazil@gmail.com>
2023-12-02 07:06:12 +0530

    packages

--- lib/screens/weather_page.dart
+++ lib/screens/weather_page.dart
@@ -10,6 +10,6 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
